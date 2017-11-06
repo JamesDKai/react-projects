13d8e2932a86f5fbe118671ea53be0cd673db20e
James Kai <James.D.Kai@gmail.com>
2017-11-06 14:50:29 -0500

    Add completed form that updates to server and state.

--- 1-contacts-page/contacts-starter/src/App.js
+++ 1-contacts-page/contacts-starter/src/App.js
@@ -41,6 +41,14 @@ class App extends Component {
 		ContactsAPI.remove(contact)
 	}
 
+	createContact(contact) {
+		ContactsAPI.create(contact).then(contact => {
+			this.setState(state => ({
+				contacts: state.contacts.concat([contact])
+			}))
+		})
+	}
+
   	render() {
 	    return (
 	      <div className="app">
@@ -51,7 +59,14 @@ class App extends Component {
 	        	/>	
 	      	)}/>
 
-	      	<Route path="/create" component={CreateContact}/>
+	      	<Route path="/create" render={({history}) => (
+				<CreateContact
+					onCreateContact={(contact) => {
+						this.createContact(contact)
+						history.push('/')
+					}}
+				/>
+			)}/>
 	      </div>
 	)
   }
